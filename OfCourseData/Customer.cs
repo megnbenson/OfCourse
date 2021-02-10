5cd593b8fc5adfff64185a05f9e260be8954ad6a
megnbenson <mbens003@gold.ac.uk>
2021-02-10 12:50:09 +0000

    reverted back so going to rebuild from create a course

--- OfCourseData/Customer.cs
+++ OfCourseData/Customer.cs
@@ -17,7 +17,7 @@ namespace OfCourseData
             City = city;
             Username = username;
             Password = password;
-            BookedCourses = new HashSet<Course>();
+            BookedCourses = new HashSet<CourseSessionDetails>();
             //LikedCourses = new HashSet<Course>();
         }
 
@@ -31,7 +31,7 @@ namespace OfCourseData
         public string LastName { get; set; }
 
 
-        public virtual ICollection<Course> BookedCourses { get; set; }
+        public virtual ICollection<CourseSessionDetails> BookedCourses { get; set; }
         //public virtual ICollection<Course> LikedCourses { get; set; }
     }
 }
---
+++ OfCourseWPF/SessionWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Shapes;
+//using OfCourseBusiness;
+
+//namespace OfCourseWPF
+//{
+//    /// <summary>
+//    /// Interaction logic for SessionWindow.xaml
+//    /// </summary>
+//    public partial class SessionWindow : Window
+//    {
+//        private CourseManager _courseManager = new CourseManager();
+//        public event EventHandler Book;
+//        private readonly MainWindow _mainWindow;
+//        private int _courseId;
+
+
+//        public SessionWindow(MainWindow mainWindow, int courseId)
+//        {
+//            _mainWindow = mainWindow;
+//            InitializeComponent();
+//            _courseId = courseId;
+//            SessionCourseTitleLabel.Content = _courseManager.GetCourseTitle(courseId);
+//        }
+
+
+//        private void Button_Click(object sender, RoutedEventArgs e)
+//        {
+
+//            _courseManager.BookCourse(_courseId, Convert.ToDateTime(availableDatesDataBinding.SelectedItem), Convert.ToDateTime(availableTimesDataBinding.SelectedItem));
+
+
+
+//            Book(this, e);
+
+
+//        }
+//    }
+//}
